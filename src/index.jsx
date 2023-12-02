5c14be318081924a1c96af4d5415530222ea4901
Derki26 <Derki26.github.com>
2023-12-01 19:46:39 -0500

    xxcx

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { 
-  BrowserRouter as Router
+  BrowserRouter as Router, RouterProvider
  } from "react-router-dom";
 import {App} from "./Components/App";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <>
-    <Router>
-      <App />
-    </Router>
-  </>,
-  rootElement
-);
+
+ReactDOM.creteRoot(document.getElementById('root')).render(
+
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+)
+
